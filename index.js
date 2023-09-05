6e62372f6aca6c769692563d2ebae3943b819f33
Frost101 <1905101@ugrad.cse.buet.ac.bd>
2023-09-05 16:06:29 +0600

    added dotenv

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 
 const app = express();
-const port = 3002;
+
+dotenv.config();
 
 // Define a route
 app.get('/', (req, res) => {
     res.render("patientHome");
 });
 
+console.log(process.env.PORT);
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:4200`);
 });
 
 /*
